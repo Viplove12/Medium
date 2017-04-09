bef5b986de56c4b21079d654a9435866fa85330c
Viplove Sharma <Viplove Sharma>
2017-04-09 17:45:56 +0530

    enquque and dequeue operations on priority queue

---
+++ test/src/package1/nayiclass.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class nayiclass {
+	public static void main(String[] args){
+		String str="7";
+		int a=Integer.parseInt(str);
+		System.out.println("conversion "+a);
+	}
+	
+
+}
